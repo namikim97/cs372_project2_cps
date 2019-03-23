78fcfb2ee062173f4652aa13b3139dc61354d865
Nami Kim <nkim6@alaska.edu>
2019-03-22 16:30:17 -0800

    Assign tasks

--- cps.h
+++ cps.h
@@ -9,37 +9,44 @@
 #ifndef CPS_H_INCLUDED
 #define CPS_H_INCLUDED
 
+// Nami 
 class shape {
 	// TO DO
 };
 
 // Basic Shapes
 
+// Nami
 class circle : public shape
 {
 	// TO DO
 }; 
 
+// Nami
 class polygon : public shape
 {
 	// TO DO
 };
 
+// Simeon
 class rectangle : public shape
 {
 	// TO DO
 }; 
 
+// Simeon
 class spacer : public shape
 {
 	// TO DO
 };
 
+// Alex
 class square : public polygon
 {
 	// TO DO
 };
 
+// Alex
 class triangle : public polygon
 {
 	// TO DO
