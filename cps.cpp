316a548d3fb9571db0cdfe7e3bc9b8071c455d77
Nami Kim <nkim6@alaska.edu>
2019-03-26 15:51:13 -0800

    Fix errors and add more functions

--- cps.cpp
+++ cps.cpp
@@ -10,158 +10,215 @@
 
 double Shape::getHeight() const
 {
-	return _height;
+    return _height;
 }
 
 double Shape::getWidth() const
 {
-	return _width;
+    return _width;
 }
 
 void Shape::setHeight(double height)
 {
-	_height = height;
+    _height = height;
 }
 
 void Shape::setWidth(double width)
 {
-	_width = width;
+    _width = width;
 }
 
-Circle::Circle(double radius)
+Circle::Circle(double radius):_radius(radius)
 {
-	_radius = radius;
-	setHeight(radius * 2);
-	setWidth(radius * 2);
+    setHeight(radius * 2);
+    setWidth(radius * 2);
 }
 
 double Circle::getRadius() const
 {
-	return _radius;
+    return _radius;
 }
 
 string Circle::getPostScriptCode() const
 {
-	string retPSCode = R"(
-		newpath
-		0 0 RAD 0 360 arc
-	)";
-
-	return retPSCode;
+    string retPSCode = R"(
+    newpath
+    0 0 RAD 0 360 arc
+    )";
+    
+    return retPSCode;
 }
 
 Polygon::Polygon(int numOfSides, double sideLength)
 {
-	_numOfSides = numOfSides;
-	_sideLength = sideLength;
-	_triHeight = calcTriHeight();
-	_triHypot = calcTriHypot();
-	_innerAngle = calcInnerAngle();
-	setHeight(calcHeight());
-	setWidth(calcWidth());
+    _numOfSides = numOfSides;
+    _sideLength = sideLength;
+    _triHeight = calcTriHeight();
+    _triHypot = calcTriHypot();
+    _innerAngle = calcInnerAngle();
+    setHeight(calcHeight());
+    setWidth(calcWidth());
 }
 
 double Polygon::calcTriHeight()
 {
-	return getTriHypot() * cos(M_PI/getNumOfSides());
+    return getTriHypot() * cos(M_PI/getNumOfSides());
 }
 
 double Polygon::calcTriHypot()
 {
-	return ( (getSideLength()/2) / (sin(M_PI/getNumOfSides())) );
+    return ( (getSideLength()/2) / (sin(M_PI/getNumOfSides())) );
 }
 
 double Polygon::calcHeight()
 {
-	int sides = getNumOfSides();
-
-	if(sides % 2 == 0)
-	{
-		return getTriHeight() * 2;
-	}
-	else
-	{
-		return getTriHeight() + getTriHypot();
-	}
+    int sides = getNumOfSides();
+    
+    if(sides % 2 == 0)
+    {
+        return getTriHeight() * 2;
+    }
+    else
+    {
+        return getTriHeight() + getTriHypot();
+    }
 }
 
 double Polygon::calcWidth()
 {
-	int sides = getNumOfSides();
-
-	if(sides == 3)
-	{
-		return getSideLength();
-	}
-
-	else if(sides % 4 == 0)
-	{
-		return getTriHeight() * 2;
-	}
-
-	else if(sides % 2 == 0)
-	{
-		return getTriHypot() * 2;
-	}
-
-	else // number of sides is odd and it's not a triangle
-	{
-		double bigTriAngle = (360/sides) * ((sides - 1)/2);
-		return 2 * getTriHypot() * sin((bigTriAngle/2) * (M_PI/180));
-	}
+    int sides = getNumOfSides();
+    
+    if(sides == 3)
+    {
+        return getSideLength();
+    }
+    
+    else if(sides % 4 == 0)
+    {
+        return getTriHeight() * 2;
+    }
+    
+    else if(sides % 2 == 0)
+    {
+        return getTriHypot() * 2;
+    }
+    
+    else // number of sides is odd and it's not a triangle
+    {
+        double bigTriAngle = (360/sides) * ((sides - 1)/2);
+        return 2 * getTriHypot() * sin((bigTriAngle/2) * (M_PI/180));
+    }
 }
 
-double Polygon::calcInnerAngle() 
+double Polygon::calcInnerAngle()
 {
-	int sides = getNumOfSides();
-	return ((sides - 2) * 180) / sides;
+    int sides = getNumOfSides();
+    return ((sides - 2) * 180) / sides;
 }
 
 int Polygon::getNumOfSides() const
 {
-	return _numOfSides;
+    return _numOfSides;
 }
 
 double Polygon::getSideLength() const
 {
-	return _sideLength;
+    return _sideLength;
 }
 
 double Polygon::getTriHeight() const
 {
-	return _triHeight;
+    return _triHeight;
 }
 
 double Polygon::getTriHypot() const
 {
-	return _triHypot;
+    return _triHypot;
 }
 
 double Polygon::getInnerAngle() const
 {
-	return _innerAngle;
+    return _innerAngle;
 }
 
-string Polygon::getPostScriptCode() const 
+string Polygon::getPostScriptCode() const
 {
-	string retPSCode = R"(  
-		newpath
-		XDRAW YDRAW moveto 
-		1 1 SIDESMINUSONE {
-			SIDELENGTH 0 rlineto 
-			ROTATIONANGLE rotate 
-		} for 
-		closepath
-	)";
-	
-	double SIDESMINUSONE = getNumOfSides() - 1;
-	double ROTATIONANGLE = 180 - getInnerAngle();
-
-	return retPSCode;
+    string retPSCode = R"(
+    newpath
+    XDRAW YDRAW moveto
+    1 1 SIDESMINUSONE {
+        SIDELENGTH 0 rlineto
+        ROTATIONANGLE rotate
+    } for
+        closepath
+        )";
+        
+        double SIDESMINUSONE = getNumOfSides() - 1;
+    double ROTATIONANGLE = 180 - getInnerAngle();
+    
+    return retPSCode;
 }
 
+Rotated::Rotated(const Shape&s, double rotation):_rotation(rotation),
+_postScriptCode(s.getPostScriptCode())
+{
+    const double radians = (_rotation * 2.0 * M_PI)/360.0;
+    
+    _origWidth = s.getWidth();
+    _origHeight = s.getHeight();
+    
+    setWidth(abs(_origWidth * cos(radians) + _origHeight * sin(radians)));
+    setHeight(abs(_origWidth * sin(radians) + _origHeight * cos(radians)));
+}
+
+string Rotated::getPostScriptCode() const
+{
+    string retPSCode;
+    
+    retPSCode = to_string((int)_rotation);
+    retPSCode += " rotate \n";
+    retPSCode += _postScriptCode;
+    
+    return retPSCode;
+}
+
+void Page::drawToPage(const Shape &s, int x, int y)
+{
+    _postScriptCode += "gsave \n";
+    _postScriptCode += to_string(x) + " " + to_string(y) + " translate\n";
+    _postScriptCode += s.getPostScriptCode();
+    _postScriptCode += "\n stroke \n grestore \n";
+}
+
+string Page::getPostScriptCode()
+{
+    return _postScriptCode;
+}
+
+void PSOutput::addPage(const Page &p)
+{
+    pages.push_back(p);
+}
+
+void PSOutput::PSOutput_File(string filename)
+{
+    ofstream of(filename);
+    of << "%%Pages: " << pages.size() << "\n";
+    of << "%1 \n /inch {72 mul} def \n";
+    int pageNumber = 1;
+    for(auto i : pages)
+    {
+        of << "%%Page: " << pageNumber << " " << pageNumber << "\n";
+        of << i.getPostScriptCode();
+        of << " \n showpage \n";
+        ++pageNumber;
+    }
+    
+    of.close();
+    cout << "Postscript Output Successful\n";
+}
 
 int main()
 {
-	return 0;
+    return 0;
 }
--- cps.h
+++ cps.h
@@ -11,34 +11,39 @@
 
 #include <string>
 using std::string;
+using std::to_string;
 #include <math.h>
 #include <vector>
+using std::vector;
+#include <cmath>
+using std::sin;
+using std::cos;
+using std::abs;
+#include<iostream>
+using std::cout;
+using std::endl;
+#include<fstream>
+using std::ofstream;
 
-// Nami 
+// Nami
 class Shape
 {
 public:
-
-	virtual double getHeight() const;
-	virtual double getWidth() const;
-	virtual string getPostScriptCode() const = 0;
-
+    virtual double getHeight() const;
+    virtual double getWidth() const;
+    virtual string getPostScriptCode() const = 0;
+    virtual ~Shape() = default;
+    
 private:
-
-	double _height;
-	double _width;
-
+    double _height;
+    double _width;
+    
 protected:
-
-	void setHeight(double height);
-	void setWidth(double width);
-
-	//for testing later
-	//string drawShape(const shape &s, int x, int y);
-
-// Nami 
-class Shape {
-	// TO DO
+    void setHeight(double height);
+    void setWidth(double width);
+    
+    //for testing later
+    //string drawShape(const shape &s, int x, int y);
 };
 
 // Basic Shapes
@@ -47,56 +52,51 @@ class Shape {
 class Circle : public Shape
 {
 public:
-
-	Circle(double radius);
-	double getRadius() const;
-	string getPostScriptCode() const override;
-
+    Circle(double radius);
+    double getRadius() const;
+    string getPostScriptCode() const override;
+    
 private:
-
-	double _radius;
-}; 
+    double _radius;
+};
 
 // Nami
 class Polygon : public Shape
 {
 public:
-
-	Polygon(int numOfSides, double sideLength);
-	double calcTriHeight();
-	double calcTriHypot();
-	double calcHeight();
-	double calcWidth();
-	double calcInnerAngle();
-	int getNumOfSides() const;
-	double getSideLength() const;
-	double getTriHeight() const;
-	double getTriHypot() const;
-	double getInnerAngle() const;
-
-	string getPostScriptCode() const override;
-
+    Polygon(int numOfSides, double sideLength);
+    double calcTriHeight();
+    double calcTriHypot();
+    double calcHeight();
+    double calcWidth();
+    double calcInnerAngle();
+    int getNumOfSides() const;
+    double getSideLength() const;
+    double getTriHeight() const;
+    double getTriHypot() const;
+    double getInnerAngle() const;
+    
+    string getPostScriptCode() const override;
+    
 private:
-
-	int _numOfSides;
-	double _sideLength;
-	double _triHeight;
-	double _triHypot;
-	double _innerAngle;
-
+    int _numOfSides;
+    double _sideLength;
+    double _triHeight;
+    double _triHypot;
+    double _innerAngle;
 };
 
 // Simeon
 class Rectangle : public Shape
 {
-
-	// TO DO
-}; 
+    
+    // TO DO
+};
 
 // Simeon
 class Spacer : public Shape
 {
-	// TO DO
+    // TO DO
 };
 
 
@@ -104,24 +104,24 @@ class Spacer : public Shape
 class Square : public Polygon
 {
 public:
-	Square(double sideLength) : _sideLength{ sideLength } {};
-
-	/*  #not sure if this fucn should return a vector<string> a stream, or something else.
-	std::vector<std::string> generatePS() {
-		std::vector<std::string> psCode[6];
-		psCode->push_back("newpath");
-		psCode->push_back()
-		
-	}
-	*/
+    //Square(double sideLength) : _sideLength{ sideLength } {};
+    
+    /*  #not sure if this fucn should return a vector<string> a stream, or something else.
+     std::vector<std::string> generatePS() {
+     std::vector<std::string> psCode[6];
+     psCode->push_back("newpath");
+     psCode->push_back()
+     
+     }
+     */
 private:
-	double _sideLength;
-;
+    double _sideLength;
+};
 
 // Alex
 class Triangle : public Polygon
 {
-	// TO DO
+    // TO DO
 };
 
 
@@ -129,35 +129,59 @@ class Triangle : public Polygon
 
 class Rotated : public Shape
 {
-	// TO DO
+public:
+    Rotated(const Shape &s, double rotation);
+    string getPostScriptCode() const override;
+    
+private:
+    double _origWidth;
+    double _origHeight;
+    double _rotation;
+    string _postScriptCode;
 };
 
 class Scaled : public Shape
 {
-	// TO DO
+    // TO DO
 };
 
 class Layered : public Shape
 {
-	// TO DO
+    // TO DO
 };
 
 class Vertical : public Shape
 {
-	// TO DO
+    // TO DO
 };
 
 class Horizontal : public Shape
 {
-	// TO DO
+    // TO DO
 };
 
 
 // postscript output
 
-class PS_Output
-{	
-	// TODO
+class Page
+{
+public:
+    void drawToPage(const Shape &s, int x, int y);
+    string getPostScriptCode();
+    
+private:
+    string _postScriptCode;
+};
+
+class PSOutput
+{
+public:
+    void addPage(const Page &p);
+    void PSOutput_File(string filename);
+    
+private:
+    string _postScriptCode;
+    vector<Page> pages;
 };
 
 #endif //CPS_H_INCLUDED
