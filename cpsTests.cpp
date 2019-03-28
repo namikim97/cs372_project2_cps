4e4a75510ebaaacd54816f408d8d7c9684c2d2f7
round-thing-over-by-that-square <lewandowski.alex@gmail.com>
2019-03-27 19:51:07 -0800

    add square test, passes

--- cpsTests.cpp
+++ cpsTests.cpp
@@ -11,5 +11,12 @@
 TEST_CASE( "Can make a polygons", "[Polygon]" ) {
     class Polygon poly(5, 6);
     REQUIRE( (poly.getNumOfSides()) == 5 );
-
+    REQUIRE( (poly.getSideLength()) == 6 );
 }
+
+TEST_CASE( "Can make a square", "[Square]" ) {
+    class Square sq(6);
+    REQUIRE( (sq.getNumOfSides()) == 4 );
+    REQUIRE( (sq.getSideLength()) == 6 );
+}
+
